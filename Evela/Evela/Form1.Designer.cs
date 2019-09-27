a23b2d7f49977d9aed443e57bede6ceb764cba5d
09anonim90 <fgs0907@mail.ru>
2019-09-27 15:02:05 +0300

    Add files via upload

---
+++ Evela/Evela/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Evela
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(24, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(568, 88);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 192);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(32, 272);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(32, 296);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(152, 344);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.Visible = false;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(32, 320);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 381);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Evela/Evela/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Evela
+{
+    public partial class Form1 : Form
+    {
+        string fpath;//путь к файлу текста
+        string fname;//файл текста
+        XmlReader xmlReader;//XmlReader обеспечивает чтение данных с xml-файла
+        string qw;// вопрос
+        string[] answ = new string[3];//Вариант ответа
+        string pic;//путь к файлу иллюстрации
+        int timeLimit = 30000;
+        int time = 0;
+        int right;//правильный ответ (номер)
+        int otv;//выбранный ответ(номер)
+        int n;//количесвто правельных ответов
+        int nv;//общее количество вопросов
+        int mode;//состояние программы
+        //0 - начало работы
+        //1 - тестирование
+        //2 - завершение работы
+
+        public Form1(string[] args)
+        {
+            InitializeComponent();
+            //Отключение кнопок выбора
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            radioButton4.Visible = false;
+
+            if (args.Length > 0)
+            {
+                if (args[0].IndexOf(";") == -1)
+                {
+                    fpath = Application.StartupPath + "\\";//Указание пути
+                    fname = args[0];//Указание имени
+                }
+                else
+                {
+                    fpath = args[0].Substring(0, args[0].LastIndexOf("\\") + 1);
+                    fname = args[0].Substring(args[0].LastIndexOf("\\") + 1);
+                }
+                try
+                {
+                    xmlReader = new XmlTextReader(fpath + fname);//Подключение xmlReader
+                    xmlReader.Read();
+                    mode = 0;//Переключение режима работы в "Начало работы"
+                    n = 0;//Обнуление правильных ответов
+                    showHead();//Запуск процедуры
+                    showDescription();//Запуск процедуры
+                }
+                catch//Обработка исключений
+                {
+                    label1.Text = "Ошибка доступа к файлу " + fpath + fname;
+                    MessageBox.Show("Ошибка доступа к файлу.\n" + fpath + fname + "\n", "Экзаменатор", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    mode = 2;//Переключение режима работы в "Завершение работы"
+                }
+            }
+            else
+            {
+                //Сообщение ои отсутствии файла
+                label1.Text = "Файл теста необходимо указать в команде запуска программы.\n Например: 'exam economics.xml' или 'exam c:\\spb.xml'.";
+                mode = 2;//Переключение режима работы в "Завершение работы"
+            }
+        }
+
+        private void showHead()
+        {
+            do xmlReader.Read();
+            while (xmlReader.Name != "head");//Чтение файла до тега "head"
+            xmlReader.Read();
+            this.Text = xmlReader.Value;
+            xmlReader.Read();
+        }
+        private void showDescription()
+        {
+            do xmlReader.Read();
+            while (xmlReader.Name != "description");//Read file until tag 'description'
+            xmlReader.Read();
+            label1.Text = xmlReader.Value;//Add text value
+            xmlReader.Read();
+            do xmlReader.Read();
+            while (xmlReader.Name != "qw");//Read file until tag 'qw'
+            xmlReader.Read();
+        }
+        private Boolean getQw()
+        {
+            xmlReader.Read();
+            if (xmlReader.Name == "q")
+            {
+                qw = xmlReader.GetAttribute("text");//Add attribute 'text'
+                pic = xmlReader.GetAttribute("src");//Add attribute 'src'
+                if (!pic.Equals(string.Empty)) pic = fpath = pic;//Picture load
+                xmlReader.Read();
+                int i = 0;
+                while (xmlReader.Name != "q")//Work with tag 'q'
+                {
+                    xmlReader.Read();
+                    if (xmlReader.Name == "a")//Work with tag 'a'
+                    {
+                        if (xmlReader.GetAttribute("right") == "yes") right = i;//Checking correct answer
+                        xmlReader.Read();
+                        if (i < 3) answ[i] = xmlReader.Value;
+                        xmlReader.Read();
+                        i++;
+                    }
+                }
+                xmlReader.Read();
+                return true;
+            }
+            else return false;
+        }
+        private void showQw()
+        {
+            label1.Text = qw;
+            if (pic.Length != 0)
+            {
+                try
+                {
+                    pictureBox1.Image = new Bitmap(pic);
+                    pictureBox1.Visible = true;
+                    radioButton1.Top = pictureBox1.Bottom + 16;//Picture show
+                }
+                catch
+                {
+                    if (pictureBox1.Visible)
+                    {
+                        pictureBox1.Visible = false;
+                        label1.Text += "\n\n\nОшибка доступа к файлу " + pic + "/";
+                        radioButton1.Top = label1.Bottom + 8;
+                    }
+                }
+            }
+        
+            else 
+            {
+                if (pictureBox1.Visible) pictureBox1.Visible = false;//Picture hide
+                radioButton1.Top = label1.Bottom;
+            }
+            radioButton1.Text = answ[0];
+            radioButton2.Top = radioButton1.Top + 24;
+            radioButton2.Text = answ[1];
+            radioButton4.Top = radioButton2.Top + 24;
+            radioButton4.Text = answ[2];
+
+            radioButton3.Checked = true;
+            button1.Enabled = false;
+        }
+        private void showLevel()
+        {
+            do xmlReader.Read();
+            while (xmlReader.Name != "levels");//Read file until tag 'levels'
+            xmlReader.Read();
+            while (xmlReader.Name != "levels")
+            {
+                xmlReader.Read();
+                if (xmlReader.Name == "levels")
+                    if (n >= Convert.ToInt32(xmlReader.GetAttribute("score")))
+                        break;
+            }
+            //Test end
+            label1.Text = "Тестирование завершено.\n" + "Всего вопросов: " + nv.ToString() + ". " + "Правельных ответов: " + n.ToString() + ".\n" + xmlReader.GetAttribute("text");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (mode)
+            {
+                case 0:
+                    //Turn on radiobuttons
+                    radioButton1.Visible = true;
+                    radioButton2.Visible = true;
+                    radioButton4.Visible = true;
+                    getQw();//Getting question
+                    showQw();//Show question
+                    mode = 1;//Switch mod to '1'
+                    button1.Enabled = false;//Disable button 1
+                    radioButton3.Checked = true;//Enable radiobutton Checking
+                    button1.Text = "Ok";//Set button text to 'Ok'
+                    timer1.Enabled = true;//Turning on timer
+                    break;
+                case 1:
+                    nv++;//Inc questions value
+                    if (otv == right) n++;//inc right questions value
+                    if (getQw()) showQw();
+                    else
+                    {
+                        //Turn off radiobuttons
+                        radioButton1.Visible = false;
+                        radioButton2.Visible = false;
+                        radioButton4.Visible = false;
+                        pictureBox1.Visible = false;
+                        showLevel();
+                        mode = 2;//switch mod to '2'
+                    }
+                    break;
+                case 2: Close(); break;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if ((RadioButton) sender == radioButton1) otv = 0;
+            if ((RadioButton) sender == radioButton2) otv = 0;
+            if ((RadioButton) sender == radioButton3) otv = 0;
+            button1.Enabled = true;//turning on button1
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            time++;
+            if (time == timeLimit) mode = 2;
+        }
+    }
+}
